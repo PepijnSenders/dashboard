ae9ed5dd874e959a2fe3b2c43c64c869a5c04267
Pepijn Senders <p.senders92@gmail.com>
2015-03-17 10:35:37 +0100

    fixed model

---
+++ src/models/Mongo/Model.php
@@ -0,0 +1,32 @@
+<?php namespace Pep\Dashboard\Models;
+
+use Jenssegers\Mongodb\Model as Moloquent;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Config;
+use Pep\Dashboard\Validation\ValidatorException;
+
+abstract class Model extends Moloquent {
+
+  protected $rules = [];
+
+  public function validate($extraRules = []) {
+    $rules = array_merge($this->rules, $extraRules);
+
+    foreach ($this->attributes as $key => $value) {
+      $properties[$key] = $this->$key;
+    }
+
+    $validator = Validator::make(
+      $properties,
+      $rules,
+      Config::get('dashboard::validation.messages', [])
+    );
+
+    $validator->getPresenceVerifier()->setConnection($this->connection);
+
+    if ($validator->fails()) {
+      throw new ValidatorException($validator->messages());
+    }
+  }
+
+}
